d88de23851b25a4716a374833f366adafe297ab1
Diego <diegoleonardo@outlook.com>
2017-10-28 21:26:28 -0200

    Config to deploy app in Heroku.

--- lib/hackaton_lead_web/channels/user_socket.ex
+++ lib/hackaton_lead_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule HackatonLeadWeb.UserSocket do
   # channel "room:*", HackatonLeadWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+  timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
