0d4badaecb0fe3661f49f6fa1dc81df058be4bc4
Diego <diegoleonardo@outlook.com>
2017-10-29 00:11:05 -0200

    testando deploy gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -18,6 +18,7 @@ load_from_system_env: true,
 url: [host: "example.com", port: 80],
 cache_static_manifest: "priv/static/cache_manifest.json"
 
+
 config :hackaton_lead, HackatonLeadWeb.Endpoint,
 server: true,
 secret_key_base: "${SECRET_KEY_BASE}"
